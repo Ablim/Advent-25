931e5dadbacf4941d3ad37a3a932208e886f53f0
Albin Bramstång <albin.bramstang@devies.se>
2025-12-04 10:05:44 +0100

    wip

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Advent-25
 Advent of Code 2025
+
+### Commands
+
+`npm install`
+
+`npm run build`
+
+`npm run aoc`
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+// const options = {
+//     hostname: 'https://adventofcode.com',
+//     path: '/2025/day/1/input',
+//     method: 'GET',
+//     headers: { 'Cookie': `session=${process.env.SESSION_COOKIE}` }
+// };
+
+
+// const response = await fetch("https://api.example.com/data");
+// const data = await response.json();
+// const cookie = process.env.SESSION_COOKIE;
+console.log(process.env.SESSION_COOKIE);
+
 console.log('Advent of Code 2025')
