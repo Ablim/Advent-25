509e9300a6b190a8bb96d69e7d721b2791ef563a
Albin Bramstång <albin.bramstang@devies.se>
2025-12-11 21:53:14 +0100

    Day 3 part 1

--- src/day3.ts
+++ src/day3.ts
@@ -1,6 +1,29 @@
 export function solvePart1(rows: string[]): string {
     let sum = 0;
 
+    rows.forEach(bank => {
+        let left = Number(bank.charAt(0));
+        let right = Number(bank.charAt(1));
+
+        for (let i = 2; i < bank.length; i++) {
+            let next = Number(bank.charAt(i));
+            let oldLeft = left;
+            let oldRight = right;
+
+            if (Number(`${oldLeft}${oldRight}`) < Number(`${oldLeft}${next}`)) {
+                left = oldLeft;
+                right = next;
+            }
+
+            if (Number(`${oldLeft}${next}`) < Number(`${oldRight}${next}`)) {
+                left = oldRight;
+                right = next;
+            }
+        }
+
+        sum += Number(`${left}${right}`);
+    });
+
     return `${sum}`;
 }
 
