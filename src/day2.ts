7b0b1ffb5f2f7a2af5396e99c1e9e30951d7c55f
Albin Bramstång <albin.bramstang@devies.se>
2025-12-11 16:52:41 +0100

    Day 2

--- src/day2.ts
+++ src/day2.ts
@@ -27,5 +27,33 @@ export function solvePart1(rows: string[]): string {
 }
 
 export function solvePart2(rows: string[]): string {
-    return ``;
+    let intervals = rows[0]?.split(",");
+
+    if (!intervals) {
+        return "";
+    }
+
+    let sum = 0;
+
+    intervals.forEach(interval => {
+        let startToEnd = interval.split("-");
+        let start = Number(startToEnd[0]);
+        let end = Number(startToEnd[1]);
+
+        for (let i = start; i <= end; i++) {
+            let value = `${i}`;
+
+            for (let j = 1; j <= value.length / 2; j++) {
+                let pattern = value.substring(0, j);
+                let regex = new RegExp(`^(${pattern}){2,}$`);
+
+                if (regex.test(value)) {
+                    sum += i;
+                    break;
+                }
+            }
+        }
+    });
+
+    return `${sum}`;
 }
