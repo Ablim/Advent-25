12d6a2689e79f4a5d2e577488d76d358f3847f0c
Albin Bramstång <albin.bramstang@devies.se>
2025-12-22 10:30:07 +0100

    Day 4

--- src/day4.ts
+++ src/day4.ts
@@ -4,11 +4,8 @@ export function solvePart1(rows: string[]): string {
 
     for (let row = 0; row < map.length; row++) {
         for (let col = 0; col < (map[0]?.length ?? 0); col++) {
-            // console.log(map[row]?.[col]);
-            if (map[row]?.[col] === "@") {
-                if (canAccess(map, row, col)) {
-                    sum++;
-                }
+            if (canAccess(map, row, col)) {
+                sum++;
             }
         }
     }
@@ -18,13 +15,47 @@ export function solvePart1(rows: string[]): string {
 
 export function solvePart2(rows: string[]): string {
     let sum = 0;
+    let map = rows.map(row => [...row]);
+    let rolls: Roll[] = [];
 
+    for (let row = 0; row < map.length; row++) {
+        for (let col = 0; col < (map[0]?.length ?? 0); col++) {
+            if (map[row]?.[col] === "@") {
+                rolls.push({
+                    row: row,
+                    col: col
+                });
+            }
+        }
+    }
 
+    let canRemove = true;
+    while (canRemove) {
+        canRemove = false;
+        let newRolls: Roll[] = [];
+
+        rolls.forEach(roll => {
+            if (canAccess(map, roll.row, roll.col)) {
+                canRemove = true;
+                map[roll.row]![roll.col] = "x";
+                sum++;
+            }
+            else {
+                newRolls.push(roll);
+            }
+        });
+
+        rolls = newRolls;
+    }
 
     return `${sum}`;
 }
 
 function canAccess(map: string[][], row: number, col: number): boolean {
+    if (map[row]?.[col] !== "@") {
+        return false;
+    }
+
     let rolls = -1;
 
     for (let i = row - 1; i <= row + 1; i++) {
@@ -44,3 +75,8 @@ function canAccess(map: string[][], row: number, col: number): boolean {
 
     return rolls < 4;
 }
+
+type Roll = {
+    row: number,
+    col: number
+};
